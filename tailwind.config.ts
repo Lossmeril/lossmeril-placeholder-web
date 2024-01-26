243d8f2dfcde3e449745541ccdda41b4d47ca2bb
Michal Špitálský <michal.spitalsky.mp@gmail.com>
2024-01-26 22:22:10 +0100

    feat: website front

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,12 +7,10 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    colors: {
+      white: "#ffffff",
+      black: "#1e1e1e",
+      green: "#48bf84"
     },
   },
   plugins: [],
